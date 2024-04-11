6f4669f76a875c0cc6f93b8394e93bfd4982de30
--Gcc-- <72279471+Gcc-Game@users.noreply.github.com>
2024-04-11 08:14:31 +0300

    Add files via upload

---
+++ Svn/Server/common/service.h or CommonDefines.h
@@ -0,0 +1,1 @@
+#define OFFICIAL_LEADER_BOOK_PLUS										// Official Leader's Book+ Plugin
---
+++ Svn/Server/game/party.cpp
@@ -0,0 +1,16 @@
+// Find
+
+	if (leader && (leader->IsEquipUniqueItem(UNIQUE_ITEM_PARTY_BONUS_EXP) || leader->IsEquipUniqueItem(UNIQUE_ITEM_PARTY_BONUS_EXP_MALL)
+		|| leader->IsEquipUniqueItem(UNIQUE_ITEM_PARTY_BONUS_EXP_GIFT) || leader->IsEquipUniqueGroup(10010)))
+	{
+		iBonusPartyExpFromItem = 30;
+	}
+
+// Add After
+
+#ifdef OFFICIAL_LEADER_BOOK_PLUS
+	if (leader && (leader->IsEquipUniqueItem(UNIQUE_ITEM_PARTY_BONUS_EXP_GOLD) || leader->IsEquipUniqueItem(UNIQUE_ITEM_PARTY_BONUS_EXP_MALL_GOLD)))
+	{
+		iBonusPartyExpFromItem = 100;
+	}
+#endif
